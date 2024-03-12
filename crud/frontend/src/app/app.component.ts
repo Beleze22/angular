453d2d34cdc0f34823b1da3bd875561474ba0dd9
GUSTAVO BELEZE <gustavo.beleze@metrosp.com.br>
2024-03-12 15:46:00 -0300

    nav não funcionando

--- crud/frontend/src/app/app.component.ts
+++ crud/frontend/src/app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Bem vindo ao {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
 }
-CD ..
---
+++ crud/frontend/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { MatToolbarModule } from '@angular/material/toolbar'; //Módulo importado para utilizar os templates disponíveis no Material
+import { MatSidenav } from '@angular/material/sidenav'
+import { MatList } from '@angular/material/list'
+
+import { HeaderComponent } from './components/template/header/header.component'; //Importando o módulo/componente gerado (ng g c)
+import { FooterComponent } from './components/template/footer/footer.component' 
+import { NavComponent } from './components/template/nav/nav.component'; 
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenav,
+    MatList
+  ],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
